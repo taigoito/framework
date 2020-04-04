7dd8c669b2176c6e441a8f3f9fe7f5f799925888
Taigo Ito <webmaster@qwel.design>
2020-04-04 09:15:02 +0900

    Fix missing echo

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -2,7 +2,7 @@
     <div id="footer-navigation">
       <div id="footer-navigation-bar">
         <div class="navigation-brand">
-          <a href="<?php home_url('/'); ?>">Qwel Design</a>
+          <a href="<?php echo home_url('/'); ?>">Qwel Design</a>
         </div>
         <?php if (has_nav_menu('secondary')) wp_nav_menu(['menu' => 'secondary', 'menu_class' => 'navigation-menu']); ?>
       </div>
--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -1,7 +1,7 @@
   <nav id="header-navigation">
     <div id="header-navigation-bar">
       <div class="navigation-brand">
-        <a href="<?php home_url('/'); ?>">Qwel Design</a>
+        <a href="<?php echo home_url('/'); ?>">Qwel Design</a>
       </div>
       <?php if (has_nav_menu('primary')) wp_nav_menu(['menu' => 'primary', 'menu_class' => 'navigation-menu']); ?>
       <button id="navigation-toggler" type="button" data-toggle="slidebar"><span data-icon="ei-navicon" data-size="m"></span></button>
